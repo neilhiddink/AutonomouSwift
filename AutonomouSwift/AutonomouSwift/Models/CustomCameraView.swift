fec9cf0f40f293ac184462379961cd8907be9b8b
Neil Hiddink <nhiddink2@gmail.com>
2019-01-06 22:26:54 -0500

    feat: Implement CustomCameraView using AVKit to Display Default Camera Feed

--- AutonomouSwift/AutonomouSwift/Models/CustomCameraView.swift
+++ AutonomouSwift/AutonomouSwift/Models/CustomCameraView.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import AVKit
 
 class CustomCameraView: UIView {
     
-    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Start up the camera
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.layer.addSublayer(previewLayer)
+        previewLayer.frame = self.bounds
+        previewLayer.connection?.videoOrientation = .landscapeRight
+    }
     
 }
